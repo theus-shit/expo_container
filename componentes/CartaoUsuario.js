a2761798eadb04b0812b8d6d53ba389af08718ca
theus-shit <dias.matheus16@escola.pr.gov.br>
2025-11-25 11:09:21 +0000

    4

--- componentes/CartaoUsuario.js
+++ componentes/CartaoUsuario.js
@@ -1,78 +1,31 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { View, Text, Image, StyleSheet} from "react-native";
 import fotos from '../img/fotos';
 
-export default function CartãoUsuario({ nome, idade, rede_social, endereco, img }) {
-
-    const lidarComClique = () => {
-        Alert.alert("Ação do Botão", `Você clicou no perfil de ${nome}!`);
-    };
-
-    return (
+export default function CartaoUsuario({nome, descricao, img}){
+    return(
         <View style={estilos.cartao}>
             <Image source={fotos[img]} style={estilos.foto} />
             <Text style={estilos.nome}>{nome}</Text>
-            <Text style={estilos.idade}>{idade}</Text>
-            <Text style={estilos.rede_social}>{rede_social}</Text>
-            <Text style={estilos.endereco}>{endereco}</Text>
-
-            <TouchableOpacity
-                style={estilos.botao}
-                onPress={lidarComClique}
-            >
-                <Text style={estilos.textoBotao}>Ver Perfil</Text>
-            </TouchableOpacity>
+            <Text style={estilos.descricao}>{descricao}</Text>
         </View>
     );
 }
 
+
 const estilos = StyleSheet.create({
-
-    cartao: {
-        margin: 10,
-        width: 340,
-        backgroundColor: '#bacfcfff',
-        padding: 60,
-        marginLeft: 50,
-        marginTop: 20,
-        borderRadius: 25,
-        alignItems: 'center',
-        elevation: 15,
-    },
-    foto: {
-        width: 80,
-        height: 80,
-        borderRadius: 40,
-        marginBottom: 10,
-    },
-
-    nome: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-
-    endereco: {
-        fontSize: 14,
-        color: '#000000ff',
-        textAlign: 'center',
-    },
-
-    botao: {
-        marginTop: 15,
-        backgroundColor: '#5b82a8',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
+     cartao:{
+        backgroundColor:'#e1f7f7',
+        padding:16,
+        marginTop:15,
+        borderRadius:10,
+        alignItems:'#000',
+        shadowOpacity: 0.1,
+        shadowOffset:{width:0, height:2},
+        shadowRadius:5,
         elevation: 5,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
     },
-
-    textoBotao: {
-        color: '#ffffff',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
+    foto:{width:80, height:80, borderRadius:40, marginBottom:10,},
+    nome:{fontSize:18, fontWeight:'bold'},
+    descricao:{fontSize:14, color: '#555', textAlign:'center'}
 });
